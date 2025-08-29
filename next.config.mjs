824ab8972f67f752174402be0eef75a686eca083
Raioni Dantas <RamotepIFPB>
2025-08-29 12:38:00 -0300

    Finalizando configuração e corrigindo deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Esta é a linha que faltava e que resolve o problema
+  output: 'export',
+
+  // Deixe o reactStrictMode, não há problema
   reactStrictMode: true,
+
+  // Configurações para o GitHub Pages
+  basePath: '/ore-miner', // MUDE AQUI para o nome do seu repositório (ex: /ore-miner)
+  assetPrefix: '/ore-miner/', // MUDE AQUI para o nome do seu repositório (ex: /ore-miner)
+
+  // Configuração para evitar erros com imagens no deploy
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
